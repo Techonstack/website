35b6a71457c8c433ee6ca4cb5cc90ed30f82166d
Tolulope Odueke <tolulope.od@gmail.com>
2021-09-13 13:50:09 +0100

    Add meta tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup landing descriptions',
-  author = 'RedQ, Inc',
+  description = 'TechOnStack is a solution oriented technology team with key focus on delivering quality solutions within estimated timelines',
+  author = 'TechOnStack Inc.',
   meta,
-  title = 'startup landing title',
+  title = 'TechOnStack',
 }) {
   const metaData = [
     {
@@ -41,9 +41,13 @@ export default function SEO({
       content: description,
     },
   ].concat(meta);
+  const faviconURL = 'techonstack_icon.png'
   return (
     <Head>
       <title>{title}</title>
+      <link rel="shortcut icon" type="image/x-icon" href={`/${faviconURL}`} />
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       {metaData.map(({ name, content }, i) => (
         <meta key={i} name={name} content={content} />
       ))}
