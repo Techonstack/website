d4656a83a0387d3da374e27f4f37f034cf15e1ae
Tolulope Odueke <tolulope.od@gmail.com>
2021-09-13 14:03:59 +0100

    add correct image url for seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -50,8 +50,8 @@ export default function SEO({
     <Head>
       <title>{title}</title>
       <link rel="shortcut icon" type="image/x-icon" href={`/${faviconURL}`} />
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:image" content="https://res.cloudinary.com/edustripe/image/upload/v1631538173/techonstack.png" />
+      <meta property="og:image" content="https://res.cloudinary.com/edustripe/image/upload/v1631538173/techonstack.png" />
       {metaData.map(({ name, content }, i) => (
         <meta key={i} name={name} content={content} />
       ))}
